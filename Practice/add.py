c32950f8750d5dc438c5947060003e3429e42d19
Alishan <mdsalishan999@gmail.com>
2022-10-01 16:08:58 +0530

    Add Program

--- Practice/add.py
+++ Practice/add.py
@@ -2,3 +2,8 @@ a=10
 b=5
 c=a+b
 print("The answer is: ",c)
+
+firstNumber=input("Enter First Number:")
+secondNumber=input("Enter Second Number:")
+sum=int(firstNumber)+int(secondNumber)
+print("THE answer is: ",sum)
